dbddd4cf81c6a93164062234a358df82a635544d
xxLockonxx <ptsgrd@naver.com>
2020-05-27 11:11:18 +0900

    Ch01 프로젝트 등록

---
+++ Ch01/src/p17/Hello.java
@@ -0,0 +1,13 @@
+package p17;
+/*
+작성자 : 이성진
+작성일 : 2020.05.07
+ */
+public class Hello {
+	//프로그램 실행 진입점
+	public static void main(String[] args) {
+		//콘솔에 출력하는 실행문
+		System.out.println("Hello. welcome to the java world!");
+		
+	} //end of main
+} //end of class
---
+++ Ch01/src/sub1/HelloWorld.java
@@ -0,0 +1,16 @@
+package sub1;
+
+/*
+ * 날짜 : 2020/04/29
+ * 이름 : 이성진
+ * 내용 : 자바 HelloWorld 출력하기
+ */
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World!");
+		System.out.println("Hello Java");
+	}
+}
