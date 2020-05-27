a7b4b2a984c86193d0199d9f3592725c06a1b95a
xxLockonxx <ptsgrd@naver.com>
2020-05-27 12:10:41 +0900

    Ch01 다시 수정

--- Ch01/src/sub1/HelloWorld.java
+++ Ch01/src/sub1/HelloWorld.java
@@ -12,5 +12,10 @@ public class HelloWorld {
 		
 		System.out.println("Hello World!");
 		System.out.println("Hello Java");
+		System.out.println("Hello Java");
+		System.out.println("Hello Java");
+		System.out.println("Hello Java");
+		System.out.println("Hello Java");
+		
 	}
 }
